7529a515d89d445c40c13962f8a4ad39f4a6b53a
Joao <joaomenezes956@gmail.com>
2026-07-03 15:49:42 -0300

    first commit

---
+++ src/main/java/com/desafiogruporevise/frete_teste/FreteTesteApplication.java
@@ -0,0 +1,13 @@
+package com.desafiogruporevise.frete_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiogruporevise/frete_teste/FreteTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiogruporevise.frete_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
