c943638062b7993c95650b62b64295b1cf084ddf
Joao <joaomenezes956@gmail.com>
2026-07-04 15:37:20 -0300

    chore: remove excecao nao utilizada

--- src/main/java/com/desafiogruporevise/frete_teste/exception/CepSemCoordenadaException.java
+++
@@ -1,7 +0,0 @@
-package com.desafiogruporevise.frete_teste.exception;
-
-public class CepSemCoordenadaException extends RuntimeException {
-    public CepSemCoordenadaException(String cep) {
-        super("Não foi possível obter coordenadas geográficas para o CEP " + cep);
-    }
-}
